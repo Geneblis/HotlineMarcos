1c5245ac3a6cdb3ef6ff7cb49d627e5b5d7f0319
Pewter14 <ianmachado09@gmail.com>
2026-05-06 17:16:58 -0300

    Básico do menu já criado
    L.A.B.U.B.U.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Nome da cena que você quer carregar ao iniciar
+    [SerializeField] private string gameSceneName = "Map01";
+
+    public void StartGame()
+    {
+        // Carrega a próxima cena
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OpenOptions()
+    {
+        Debug.Log("Abrindo Opções...");
+        // Futuramente você pode ativar um painel de opções aqui
+    }
+
+    public void OpenCredits()
+    {
+        Debug.Log("Abrindo Créditos...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Sair do Jogo");
+        // Funciona apenas no jogo buildado (executável)
+        Application.Quit();
+    }
+}
